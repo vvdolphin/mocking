5c03c3771930dee7cf122f0357fec1d6a0152233
WANGZO <1273027520A@qq.com>
2020-08-06 21:04:34 +0800

    [todo_list][Benjamin&Zoe] feature: add repository and table in flyway

---
+++ todo_list_pro/src/main/java/com/thoughtworks/responsity/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.responsity;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TodoRepository {
+}
---
+++ todo_list_pro/src/main/resources/db/migration/V20200806205933__create_todo_db.sql
@@ -0,0 +1,5 @@
+create table todo(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255),
+    status  boolean DEFAULT false
+)
