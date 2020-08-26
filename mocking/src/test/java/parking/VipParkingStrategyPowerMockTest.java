a4c62e4f8a046548fbbdea24434294809b05935c
WANGZO <1273027520A@qq.com>
2020-08-26 18:25:43 +0800

    [Jack & Zoe]implement:testIsAllowOverPark_givenCarNameContainsCharacterAAndIsNotVipCar_thenReturnFalse

--- mocking/src/test/java/parking/VipParkingStrategyPowerMockTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyPowerMockTest.java
@@ -2,12 +2,16 @@ package parking;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Calendar;
 
 import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
+//import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(value = {ParkingLot.class})
@@ -20,8 +24,9 @@ public class VipParkingStrategyPowerMockTest {
      * by using PowerMock to mock static method */
     // given
     VipParkingStrategy vipParkingStrategy = new VipParkingStrategy();
-    mockStatic(ParkingLot.class);
-    when(ParkingLot.getBasicHourlyPrice()).thenReturn(25);
+    mockStatic(ParkingLot.class, Calendar.class);
+    when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)).thenReturn(Calendar.SUNDAY);
+    when(ParkingLot.getBasicHourlyPrice()).thenCallRealMethod();
     // when
     int result = vipParkingStrategy.calculateHourlyPrice();
     // then
@@ -34,11 +39,12 @@ public class VipParkingStrategyPowerMockTest {
     /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
      * by using PowerMock to mock static method */
     VipParkingStrategy vipParkingStrategy = new VipParkingStrategy();
-    mockStatic(ParkingLot.class);
-    when(ParkingLot.getBasicHourlyPrice()).thenReturn(20);
+    mockStatic(ParkingLot.class, Calendar.class);
+    when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)).thenReturn(Calendar.MONDAY);
+    when(ParkingLot.getBasicHourlyPrice()).thenCallRealMethod();
     // when
     int result = vipParkingStrategy.calculateHourlyPrice();
     // then
-    assertEquals(20, result);
+    assertEquals(40, result);
   }
 }
--- mocking/src/test/java/parking/VipParkingStrategyTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyTest.java
@@ -78,8 +78,8 @@ public class VipParkingStrategyTest {
      * You may refactor the code, or try to use
      * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
      */
-    when(carDao.isVip(anyString())).thenReturn(false);
-    Car car = new Car("Ase");
+    when(carDao.isVip(anyString())).thenReturn(true);
+    Car car = new Car("se");
     ParkingLot parkingLot = new ParkingLot("south", 1);
     List<ParkingLot> parkingLots = new ArrayList<>();
     parkingLots.add(parkingLot);
@@ -95,10 +95,13 @@ public class VipParkingStrategyTest {
      * You may refactor the code, or try to use
      * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
      */
-
-
-
-
+    when(carDao.isVip(anyString())).thenReturn(false);
+    Car car = new Car("Ase");
+    ParkingLot parkingLot = new ParkingLot("south", 1);
+    List<ParkingLot> parkingLots = new ArrayList<>();
+    parkingLots.add(parkingLot);
+    boolean allowOverPark = vipParkingStrategy.isAllowOverPark(car);
+    Assert.assertEquals(false,allowOverPark);
   }
 
   @Test
